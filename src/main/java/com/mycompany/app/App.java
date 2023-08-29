c1144c5827265c5f33ff83428961d8238b376cd7
AnushaRoopireddy <anuroopireddy@gmail.com>
2023-08-29 14:56:10 +0530

    dockerfile added

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,5 +9,13 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+//code changes in 103
+
+int a =20;
+int b =30;
+int c = a+b;
+system.out.println(c);
+
     }
 }
